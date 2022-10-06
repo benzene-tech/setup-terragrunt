36c1c7b88b9536ce650af4f743753d10a16593f5
Santhosh <hsohtnas001@gmail.com>
2022-10-06 17:03:32 +0530

    Setup terragrunt with specific version

--- README.md
+++ README.md
@@ -1,9 +1,44 @@
 # Setup Terragrunt
 
-This action setup terragrunt in linux runner.
+This action installs terragrunt CLI in runners.
 
 ## Usage
 
 ```yaml
-- uses: SanthoshNath/setup-terragrunt
+- uses: SanthoshNath/setup-terragrunt@v2
+  with:
+    # Terragrunt version to be installed. Otherwise, installs latest version
+    # For example, 0.38.10
+    version: ''
+    # Token to get latest release.
+    # Default: ${{ github.token }}
+    token: ''
+```
+
+## Scenarios
+
+- [Install latest version](#install-latest-version)
+- [Install specifc version](#install-specifc-version)
+- [Install using Personal access token](#install-using-personal-access-token)
+
+### Install latest version
+
+```yaml
+- uses: SanthoshNath/setup-terragrunt@v2
+```
+
+### Install specifc version
+
+```yaml
+- uses: SanthoshNath/setup-terragrunt@v2
+  with:
+    version: 0.38.10
+```
+
+### Install using Personal access token
+
+```yaml
+- uses: SanthoshNath/setup-terragrunt@v2
+  with:
+    token: ${{ secrets.TOKEN }}
 ```
--- index.js
+++ index.js
@@ -1,11 +1,60 @@
-const core = require('@actions/core');
-const { execSync } = require("child_process");
-const { homedir } = require('os');
+const core = require('@actions/core')
+const github = require('@actions/github')
+const tc = require('@actions/tool-cache')
+const { execSync } = require("child_process")
 
-try {
-    core.addPath('/home/linuxbrew/.linuxbrew/bin')
-    execSync('brew install terragrunt');
+async function run() {
+    try {
+        const platform = {
+            linux: 'linux',
+            darwin: 'darwin',
+            win32: 'windows'
+        };
+
+        const arch = {
+            x64: 'amd64',
+            arm64: 'arm64'
+        }
+
+        const token = core.getInput('token')
+        if (token === '')
+            throw new Error('token undefined')
+
+        version = core.getInput('version')
+        if (version === '') {
+            const octokit = github.getOctokit(token)
+
+            version = await octokit.rest.repos.getLatestRelease({
+                owner: 'gruntwork-io',
+                repo: 'terragrunt'
+            }).then(result => {
+                return result.data.tag_name
+            })
+        }
+        else
+            version = `v${version}`
+
+        const terragruntPath = tc.find('Terragrunt', version);
+        if (terragruntPath !== '') {
+            core.info(`terragrunt with ${version} already installed`)
+            core.addPath(terragruntPath);
+            return
+        }
+
+        const suffix = process.platform === 'win32' ? '.exe' : ''
+        const pathToCLI = await tc.downloadTool(`https://github.com/gruntwork-io/terragrunt/releases/download/${version}/terragrunt_${platform[process.platform]}_${arch[process.arch]}${suffix}`)
+
+        if (process.platform !== 'win32')
+            execSync(`chmod u+x ${pathToCLI}`)
+
+        const cachedPath = await tc.cacheFile(pathToCLI, `terragrunt${suffix}`, 'Terragrunt', version)
+        core.addPath(cachedPath)
+
+        core.setOutput('version', version)
+    }
+    catch (error) {
+        core.setFailed(error.message)
+    }
 }
-catch (error) {
-    core.setFailed(error.message);
-}
+
+run()
