769ce95473098dffd58ce606aa5e86ddb2d742ff
Santhosh <santhoshsivanath@outlook.com>
2023-09-12 12:11:29 +0530

    Refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,11 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
-import * as tc from "@actions/tool-cache";
-import * as exec from "@actions/exec";
-import * as io from "@actions/io";
+import * as core from "@actions/core"
+import * as github from "@actions/github"
+import * as tc from "@actions/tool-cache"
+import * as exec from "@actions/exec"
+import * as io from "@actions/io"
 
 async function run() {
-    const platform = {
-        linux: `linux`,
-        darwin: `darwin`,
-        win32: `windows`
-    }
-    const arch = {
-        x64: `amd64`,
-        arm64: `arm64`
-    }
-
     const token = core.getInput(`token`)
-    const installWrapper = core.getBooleanInput(`install_wrapper`)
-
     const octokit = github.getOctokit(token)
 
     let tag = core.getInput(`version`)
@@ -47,11 +35,20 @@ async function run() {
 
     const terragruntPath = tc.find(`Terragrunt`, tag)
     if (terragruntPath !== ``) {
-        core.notice(`terragrunt with ${tag} already installed`)
+        core.notice(`Terragrunt with ${tag} already installed`)
         core.addPath(terragruntPath)
         return
     }
 
+    const platform = {
+        linux: `linux`,
+        darwin: `darwin`,
+        win32: `windows`
+    }
+    const arch = {
+        x64: `amd64`,
+        arm64: `arm64`
+    }
     const cliSuffix = process.platform === `win32` ? `.exe` : ``
     const pathToCLI = await tc.downloadTool(`https://github.com/gruntwork-io/terragrunt/releases/download/${tag}/terragrunt_${platform[process.platform as keyof Object]}_${arch[process.arch as keyof Object]}${cliSuffix}`)
     if (process.platform !== `win32`) {
@@ -63,6 +60,7 @@ async function run() {
     const cachedPath = await tc.cacheFile(pathToCLI, `terragrunt${cliSuffix}`, `Terragrunt`, tag)
     await io.rmRF(pathToCLI)
 
+    const installWrapper = core.getBooleanInput(`install_wrapper`)
     if (installWrapper) {
         await exec.exec(`npm link`, [], {
             silent: true
@@ -79,6 +77,9 @@ async function run() {
     core.info(`Installed Terragrunt version: ${version}`)
 }
 
-run().catch(error => {
-    core.setFailed(error)
-})
+
+if (require.main === module) {
+    run().catch(error => {
+        core.setFailed(error)
+    })
+}
